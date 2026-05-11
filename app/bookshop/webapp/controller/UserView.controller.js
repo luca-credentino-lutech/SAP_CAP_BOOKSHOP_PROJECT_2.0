7d8f7bb2808d6d53a23e48bf53c15ff45c45979a
luca-credentino-lutech <luca.credentino@lutech.it>
2026-05-11 17:58:57 +0200

    Funzionalità carrello: sistemare l'acquisto

--- app/bookshop/webapp/controller/UserView.controller.js
+++ app/bookshop/webapp/controller/UserView.controller.js
@@ -2,16 +2,15 @@ sap.ui.define([
     "sap/ui/core/mvc/Controller",
     "sap/ui/model/json/JSONModel",
     "sap/m/MessageToast",
-    "sap/m/MessageBox"
-], (Controller, JSONModel, MessageToast, MessageBox) => {
+    "sap/m/MessageBox",
+    "sap/ui/core/Fragment"
+], (Controller, JSONModel, MessageToast, MessageBox, Fragment) => {
     "use strict";
 
     return Controller.extend("bookshop.controller.UserView", {
         onInit() {
-            // debugger
-            // let oTable = this.byId("tableUser")
-            // let oBinding = oTable.getModel().getBindingContext().getObject()
-
+            debugger
+ 
         },
         // async onSelectionChangeLibro() {
         //     debugger
@@ -45,21 +44,99 @@ sap.ui.define([
             const oItem = oEvent.getParameter("listItem");
             const oLibro = oItem.getBindingContext().getObject();
 
-            if (oLibro.stock == 0) {
+            if (oLibro.stock === 0) {
                 oTable.setSelectedItem(oItem, false);
                 MessageToast.show(`${oLibro.titolo} non è presente in magazzino al momento`);
                 return;
             }
 
         },
-  
-        onLogOff() {
-            localStorage.clear();
-            sessionStorage.clear()
-            this.getOwnerComponent().getRouter().navTo("RouteView1");
 
+        InserisciCarrello() {
+            const oTable = this.byId("tableUser");
+            const aLibriSelezionati = oTable.getSelectedItems().map(oItem => {
+                const oCarrelloLibri = oItem.getBindingContext().getObject()
+
+                return {
+
+                    ID: oCarrelloLibri.ID,
+                    titolo: oCarrelloLibri.titolo,
+                    descrizione: oCarrelloLibri.descrizione,
+                    quantita: 1
+
+
+                }
+            }
+            )
+
+            const oModelCarrello = new JSONModel({
+                items: aLibriSelezionati
+            });
+
+
+            this.ApriCarrello(oModelCarrello);
+        },
+        onChiudiCarrello() {
+            this.byId("carrelloDialog").close();
         },
 
+        ApriCarrello(oModelCarrello) {
+            const oView = this.getView();
+            oView.setModel(oModelCarrello, "CarrelloLibri")
+            if (!this.Carrello) {
+                this.Carrello = Fragment.load({
+                    id: oView.getId(),
+                    name: "bookshop.fragment.Carrello",
+                    controller: this
+                }).then((oDialog) => {
+                    oView.addDependent(oDialog);
+                    return oDialog;
+                });
+            }
+
+            this.Carrello.then((oDialog) => {
+                oDialog.open();
+            });
+        },
+
+        onAcquistaCarrello(oEvent){
+
+        const oModel = this.getView().getModel("CarrelloLibri");
+        const oLibri = oModel.getData().items
+        // oLibri.map(i => {
+        //     this.AcquistaFetch(i.ID, i.quantita)
+        // })
+          
+        },
+
+        onItemActionPress(oEvent) {
+            const oModel = this.getView().getModel("CarrelloLibri");
+
+            const oItem = oEvent.getParameter("listItem");
+
+            const oAction = oEvent.getParameter("action");
+
+            const oContext = oItem.getBindingContext("CarrelloLibri");
+
+            const sPath = oContext.getPath();
+
+            const iQuantitaAttuale = oModel.getProperty(sPath + "/quantita");
+
+
+            if (oAction.getText() == "Aggiungi") {
+                oModel.setProperty(sPath + "/quantita", iQuantitaAttuale + 1);
+            }
+
+            if (oAction.getText() == "Leva") {
+                const iNuovaQuantita = iQuantitaAttuale - 1;
+                oModel.setProperty(sPath + "/quantita", iNuovaQuantita);
+
+                if(iNuovaQuantita == 0){
+                    oItem.mProperties = {}
+                }
+                
+            }
+        },
         async AcquistaLibro() {
             const oTable = this.byId("tableUser");
             const aSelectedItems = oTable.getSelectedItems();
